4504cf6398a84d90c531ae5246c4cdb45d03df1d
Daneric <danericaalbers@hotmail.com>
2022-05-16 12:56:14 +0200

    feat: comments

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express();
 const chalk = require('chalk');
 
+// load the api route from the routes folder with the route prefix /api
 router.use('/api', require('./routes/api'));
 console.log(chalk.blue('[API]') + ' ' + chalk.yellow('Routes loaded'));
 
+// Start the server
 router.listen(3000, () => {
     console.log(chalk.blue('[API]') + ' ' + chalk.yellow('Server started on port 3000'));
 });
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+// basic route at /api
 app.get('/', (req, res) => {
     res.send({
         message: 'Hello World!'
     });
 });
 
+// export the app
 module.exports = app;
