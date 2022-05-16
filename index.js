32497a1d58e7cd77e23853ccf0ae7873c69b7196
Daneric <danericaalbers@hotmail.com>
2022-05-16 13:09:56 +0200

    feat: import config

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express();
 const chalk = require('chalk');
+const config = require('./middleware/config');
 
 // load the api route from the routes folder with the route prefix /api
 router.use('/api', require('./routes/api'));
 console.log(chalk.blue('[API]') + ' ' + chalk.yellow('Routes loaded'));
 
 // Start the server
-router.listen(3000, () => {
+router.listen(config.port, () => {
     console.log(chalk.blue('[API]') + ' ' + chalk.yellow('Server started on port 3000'));
 });
