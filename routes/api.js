7bbaaf3491d450b4a2c7e74e4fa9dc9c7e062610
Daneric <danericaalbers@hotmail.com>
2022-05-18 12:08:53 +0200

    feat: give front-end access

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,24 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+// Set app headers to allow localhost:8080 to acces the api 
+app.use(cors({
+    origin: 'http://localhost:8080',
+    credentials: true
+}));
+
 
 // basic route at /api
 app.get('/', (req, res) => {
     res.send({
-        message: 'Hello World!'
+        data: {
+            message: 'Welcome to the API',
+            status: 'success'
+        }
     });
 });
+
 
 // export the app
 module.exports = app;
