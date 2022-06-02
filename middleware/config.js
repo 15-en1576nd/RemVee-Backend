c66c6b2c47710889f751610d683f806d27126968
Martvdm <martvandermolen@gmail.com>
2022-06-02 10:11:52 +0200

    feat: database vars in config middleware

--- middleware/config.js
+++ middleware/config.js
@@ -4,6 +4,14 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const config = {
     port: process.env.PORT || 3000,
     allowed: process.env.ALLOWED || 'http://localhost:8080',
+    // Database connection
+    db: {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 3306,
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_DATABASE || 'remvee'
+    },
 }
 
 // export the config
