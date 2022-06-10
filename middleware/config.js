36f5f4d84ae40a3f937abaa6daee269ab8851aef
Martvdm <martvandermolen@gmail.com>
2022-06-10 08:44:50 +0200

    feat: db array in config.js

--- middleware/config.js
+++ middleware/config.js
@@ -6,13 +6,23 @@ const config = {
     allowed: process.env.ALLOWED || 'http://localhost:8080',
     // Database connection
     db: {
+        client: process.env.DB_CLIENT || 'mysql',
+        connection: {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_DATABASE || 'remvee'
+        password: process.env.DB_PASS || '',
+        database: process.env.DB_NAME || 'remvee',
     },
-}
+    migrations: {
+        directory: path.resolve(__dirname, '../database/migrations'),
+        tableName: 'migrations'
+    },
+    seeds: {
+        directory: path.resolve(__dirname, '../database/seeds'),
+    },
+    },
+};
 
 // export the config
 module.exports = config;
