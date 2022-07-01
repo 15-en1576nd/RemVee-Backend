5ce4f10fc565c8376ddfd7f57f0d7e10827d1724
Martvdm <martvandermolen@gmail.com>
2022-07-01 13:52:29 +0200

    feat: Tv discover api route

--- routes/TvController.js
+++ routes/TvController.js
@@ -12,19 +12,33 @@ app.use(cors({
     credentials: true
 }));
 
-app.post('/tv/status', async (req, res) => {
+// Send commands to tv on a specific ip.
+app.post('/tv/command', async (req, res) => {
     let ipaddress = req.body.ipaddress; // Get ipaddress from request
-    let method = req.body.method; // Get method from request
-    let tv = new Bravia(ipaddress, "80", "0000"); // Create new tv object with ipaddress and port.
-    await tv.system.invoke(method)
-    .then(result => res.status(200).send({message: 'Bravia tv ' + method + ' requested', ipaddress: ipaddress, result: result}))
-    .catch(err => res.status(400).send({message: 'Command did not send to Bravia tv', ipaddress: ipaddress, command: command, error: err}));
+    const command = req.body.command; // Command that will be send to the tv.
+    const tv = new Bravia(ipaddress, "80", "0000"); // Create new tv object with ipaddress and port.
+    await tv.send(command)
+    .then(_ => res.status(200).send({message: 'Command send to Bravia tv', ipaddress: ipaddress, command: command}))
+    .catch(err => res.status(400).send({message: err}));
+});
 
-}
-);
+    app.post('/tv/status', async (req, res) => {
+        let ipaddress = req.body.ipaddress; // Get ipaddress from request
+        let method = req.body.method; // Get method from request
+        let tv = new Bravia(ipaddress, "80", "0000"); // Create new tv object with ipaddress and port.
+        await tv.system.invoke(method)
+        .then(result => res.status(200).send({message: 'Bravia tv ' + method + ' requested', ipaddress: ipaddress, result: result}))
+        .catch(err => res.status(400).send({message: 'Command did not send to Bravia tv', ipaddress: ipaddress, command: command, error: err}));
+
+    }
+    );
 
 
 app.post('/tv/discover', async (req, res) => {
+    await Bravia.discover(10000)
+    .then(tvs => res.status(200).send({message: 'Tv\'s found', tvs}))
+    .catch(err => res.status(400).send({message: err}));
+});
 
 // export the app
 module.exports = app;
