ac60bd62554f1e376bc99f7a8ac392fd67791dee
Martvdm <martvandermolen@gmail.com>
2022-07-01 13:52:06 +0200

    feat: Get status tv api route

--- routes/TvController.js
+++ routes/TvController.js
@@ -12,6 +12,19 @@ app.use(cors({
     credentials: true
 }));
 
+app.post('/tv/status', async (req, res) => {
+    let ipaddress = req.body.ipaddress; // Get ipaddress from request
+    let method = req.body.method; // Get method from request
+    let tv = new Bravia(ipaddress, "80", "0000"); // Create new tv object with ipaddress and port.
+    await tv.system.invoke(method)
+    .then(result => res.status(200).send({message: 'Bravia tv ' + method + ' requested', ipaddress: ipaddress, result: result}))
+    .catch(err => res.status(400).send({message: 'Command did not send to Bravia tv', ipaddress: ipaddress, command: command, error: err}));
+
+}
+);
+
+
+app.post('/tv/discover', async (req, res) => {
 
 // export the app
 module.exports = app;
